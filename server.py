c8ce7318a312259c05e9d892cdc1adc750d82e62
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-01-15 12:39:47 -0800

    Update login / register system

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 from mysqlconnection import MySQLConnector
 import os, md5, binascii, datetime
 
@@ -10,10 +10,13 @@ def setup_session():
 	if not "username" in session:
 		session["username"] = None
 
+	if not "password_hash" in session:
+		session["password_hash"] = None
+
 @app.route("/")
 def index():
 	setup_session()
-	messages = mysql.query_db("SELECT * FROM messages ORDER BY created_at DESC")
+	messages = mysql.query_db("SELECT * FROM messages ORDER BY id DESC")
 
 	return render_template("index.html", messages=messages)
 
@@ -23,28 +26,48 @@ def index():
 def login():
 	setup_session()
 
-	return render_template("login.html")
+	if session["username"] == None:
+		return render_template("login.html")
+
+	return render_template("message.html", message="You are already logged in.")
 
 @app.route("/login/process", methods=["POST"])
 def process_login():
-	if True: #successful login
-		setup_session()
+	select_query = "SELECT * FROM users WHERE username = :username"
+	select_data = { "username": request.form["username"] }
 
-		session["username"] = request.form["username"]
+	row_data = mysql.query_db(select_query, select_data)
 
-	return render_template("login.html", success=True)
+	if len(row_data) > 0:
+		password_hash = md5.new(request.form["password"] + row_data[0]["password_salt"]).hexdigest()
+
+		if password_hash == row_data[0]["password_hash"]: # Successful login
+			success = True
+
+			setup_session()
+
+			session["username"] = request.form["username"]
+			session["password_hash"] = password_hash
+
+			return redirect("/login/success")
+
+	return render_template("login.html", message="Invalid log-in details.")
+
+@app.route("/login/success")
+def successful_login():
+	return render_template("message.html", message="Successfully logged in.")
 
 ################## REGISTRATION ##################
 
 @app.route("/register")
 def register():
-	setup_session()
+	if session["username"] == None:
+		return render_template("register.html")
 
-	return render_template("register.html")
+	return render_template("message.html", message="You are already logged in.")
 
 @app.route("/register/process", methods=["POST"])
 def process_register():
-	success = False
 	select_query = "SELECT * FROM users WHERE username = :username OR email = :email"
 
 	select_data = {
@@ -53,8 +76,6 @@ def process_register():
 	}
 
 	if len(mysql.query_db(select_query, select_data)) == 0: # Valid registration
-		success = True
-
 		insert_query = "INSERT INTO users (username, password_hash, password_salt, email, created_at, updated_at) "
 		insert_query += "VALUES (:username, :password_hash, :password_salt, :email, :now, :now)"
 
@@ -73,8 +94,15 @@ def process_register():
 		setup_session()
 
 		session["username"] = request.form["username"]
+		session["password_hash"] = insert_data["password_hash"]
 
-	return render_template("register.html", success=success)
+		return redirect("/register/success")
+
+	return render_template("register.html", message="Account with username or email already exists.")
+
+@app.route("/register/success")
+def successful_register():
+	return render_template("message.html", message="Successfully registered.")
 
 @app.route("/logout")
 def logout():
